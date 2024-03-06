63fe52534c13e55d7c4e8b6f7732b41917dde6ed
FloFlo-L <florian.lescribaa@gmail.com>
2024-03-06 12:34:28 +0100

    translated_text

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from pytube import YouTube
 import whisper
 import os
 import ssl
-from google.cloud import translate_v2 as translate
+from googletrans import Translator, LANGUAGES
 
-# Set the path to your API key JSON file
 api_key = 'AIzaSyBserMzRBI--CPUQBg0hU5nVkIO81mwPFU'
 
 app = FastAPI()
@@ -19,18 +18,16 @@ def download_audio_from_youtube(url: str) -> str:
 
 # Fonction pour transcrire l'audio en texte avec Whisper
 def transcribe_audio(audio_file: str) -> str:
-    model = whisper.load_model("base")
+    model = whisper.load_model("medium")
     result = model.transcribe(audio_file)
     os.remove(audio_file)  # Supprimer le fichier audio temporaire
     return result['text']  # Texte transcrit de l'audio
 
 # Fonction pour traduire du texte dans une langue
 def translate_text(text: str, lang: str) -> str:
-    # Create an instance of the Translator class
-    translate_client = translate.Client()
-    # Use the translate() method to translate the text
-    translation = translate_client.translate(text, target_language=lang)
-    return translation['translatedText']
+    translator = Translator()
+    translated_text = translator.translate(text, dest=lang).text
+    return translated_text
 
 @app.post("/translate/")
 async def translate_video(url: str, lang: str):
@@ -39,10 +36,10 @@ async def translate_video(url: str, lang: str):
         audio_file = download_audio_from_youtube(url)
         # Transcrire l'audio en texte
         transcription = transcribe_audio(audio_file)
+        print(transcription)
         # Traduire le texte dans une langue
         translation = translate_text(transcription, lang)
         return {
-            "transcription": transcription,
             "translation": translation
         }
     except Exception as e:
