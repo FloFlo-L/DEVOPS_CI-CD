89e4186b2f696f492313484d8f6386bef4211b20
Axel verglas <axelverglas@outlook.com>
2024-04-15 14:23:39 +0200

    Mise à jour du contrôleur de traduction pour utiliser l'API DeepL

--- services/translation.py
+++ services/translation.py
@@ -1,5 +1,18 @@
-from translate import Translator
+import deepl
+from dotenv import load_dotenv
+import os
+
+# Charge les variables d'environnement du fichier .env
+load_dotenv()
+
+# Récupère la clé API de l'environnement
+auth_key = os.getenv("API_DEEPL")
+translator = deepl.Translator(auth_key)
 
 def translate_text(text: str, lang: str) -> str:
-    translator = Translator(to_lang=lang)
-    return translator.translate(text)
+    try:
+        translation = translator.translate_text(text, target_lang=lang)
+        return translation.text
+    except Exception as e:
+        print(f"Error during translation: {e}")
+        raise
